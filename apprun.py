586181f84936ae2bc22c032979ca468f5e9b1926
mzlaw <chaosdevil112@gmail.com>
2023-03-28 11:52:08 +0800

    application run

--- apprun.py
+++ apprun.py
@@ -1,6 +1,4 @@
-# @copyright Copyright (C) 2023 UTAR Futuretech. Blockchain x Computer Science Club
-# @author    Orion <chaosdevil112@gmail.com>
-# @license   Proprietary
+# application run
 
 from route.admin import api_admin
 from route.frontend import api_frontend
